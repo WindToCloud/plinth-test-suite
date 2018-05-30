d473fb34e07892998b4b554ed3c45f29f30b05e5
chenjing <chenjing@123.com>
2018-05-30 10:00:27 +0000

    debug hns in d06

--- auto-test/smoke/xge_autotest/case_script/auto_negotiation_support.sh
+++ auto-test/smoke/xge_autotest/case_script/auto_negotiation_support.sh
@@ -14,7 +14,7 @@ function ge_restarts_auto_negotiation()
     echo "Begin to ssh remote "${remote_tp1}
     ssh root@${BACK_IP} 'ifconfig '${remote_tp1}' up; ifconfig '${remote_tp1}' '${remote_tp1_ip}'; sleep 5;'
     i=1
-    MESSAGE="PASS"
+    # MESSAGE="PASS"
     while(($i<=10))
     do
 	echo "Begin cycle "$i
@@ -23,7 +23,7 @@ function ge_restarts_auto_negotiation()
         ethtool -r ${local_tp1}
         sleep 5
         ping ${remote_tp1_ip} -c 5 > ${HNS_TOP_DIR}/data/log/ge_restarts_auto_negotiation.txt &
-        sleep 10    
+        sleep 10
         cat ${HNS_TOP_DIR}/data/log/ge_restarts_auto_negotiation.txt | grep "received, 0% packet loss" >/dev/null
         if [ $? -eq 0 ];then
             enableok=1
@@ -40,17 +40,17 @@ function ge_restarts_auto_negotiation()
         if [ $? -eq 0 ];then
             disableok=1
         fi
-	echo "The result of Enable net is "$enableok
-	echo "The result of disable net is "$disableok
+        echo "The result of Enable net is "$enableok
+        echo "The result of disable net is "$disableok
 
         if [ $enableok -eq 0 ] || [ $disableok -eq 0 ];then
             MESSAGE="FAIL\t auto negotiation fail"
-	    echo ${MESSAGE}
+	        echo ${MESSAGE}
             break
         fi
         i=$(($i+1))
     done
-    #MESSAGE="PASS"
+    MESSAGE="PASS"
     echo ${MESSAGE}
 }
 
@@ -68,7 +68,7 @@ function ge_iperf_auto_negotiation()
         sleep 10
     done
     ping ${remote_tp1_ip} -c 5 > ${HNS_TOP_DIR}/data/log/ge_ping_auto_negotiation.txt &
-    sleep 10    
+    sleep 10
     cat ${HNS_TOP_DIR}/data/log/ge_ping_auto_negotiation.txt | grep "received, 0% packet loss" >/dev/null
     if [ $? -eq 0 ];then
         enableok=1
@@ -79,12 +79,13 @@ function ge_iperf_auto_negotiation()
         killall iperf
         ssh root@${BACK_IP} "killall iperf"
         MESSAGE="PASS"
-	echo ${MESSAGE}
+	    echo ${MESSAGE}
     else
         killall iperf
         ssh root@${BACK_IP} "killall iperf"
         MESSAGE="FAIL\t iperf auto negotiation fail"
-	echo ${MESSAGE}
+	    echo ${MESSAGE}
+        return 1
     fi
 }
 
@@ -96,8 +97,8 @@ function xge_restarts_auto_negotiation()
     echo "Begin to run "${Test_Case_Title}
     ifconfig ${local_fibre1} up; ifconfig ${local_fibre1} ${local_fibre1_ip}
     ssh root@${BACK_IP} "ifconfig "${remote_tp1}" up; ifconfig "${remote_fibre1}" "${remote_fibre1_ip}"; sleep 5"
-     MESSAGE="PASS"
-   
+    # MESSAGE="PASS"
+
     i=1
     while(($i<=10))
     do
@@ -106,7 +107,7 @@ function xge_restarts_auto_negotiation()
         ethtool -r ${local_fibre1}
         sleep 5
         ping ${remote_fibre1_ip} -c 5 > ${HNS_TOP_DIR}/data/log/xge_restarts_auto_negotiation.txt &
-        sleep 10    
+        sleep 10
         cat ${HNS_TOP_DIR}/data/log/xge_restarts_auto_negotiation.txt | grep "received, 0% packet loss" >/dev/null
         if [ $? -eq 0 ];then
             enableok=1
@@ -125,11 +126,11 @@ function xge_restarts_auto_negotiation()
         if [ $enableok -eq 0 ] || [ $disableok -eq 0 ];then
             MESSAGE="FAIL\t auto negotiation fail"
             echo ${MESSAGE}
-	    break
+            break
         fi
         i=$(($i+1))
     done
-   # MESSAGE="PASS"
+    MESSAGE="PASS"
     echo ${MESSAGE}
 }
 
@@ -146,7 +147,7 @@ function xge_iperf_auto_negotiation()
         sleep 10
     done
     ping ${remote_fibre1_ip} -c 5 > ${HNS_TOP_DIR}/data/log/xge_ping_auto_negotiation.txt &
-    sleep 10    
+    sleep 10
     cat ${HNS_TOP_DIR}/data/log/xge_ping_auto_negotiation.txt | grep "received, 0% packet loss" >/dev/null
     if [ $? -eq 0 ];then
         enableok=1
