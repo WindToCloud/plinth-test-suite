068fa88b98f9fed153151eb2da746ec6d99f956d
Luojiaxing1991 <luojiaxing@huawei.com>
2018-05-30 20:37:08 +0800

    debug mail 2

--- auto-test/smoke/pre_autotest/pre_main.sh
+++ auto-test/smoke/pre_autotest/pre_main.sh
@@ -13,6 +13,8 @@ commit_id=`cat /proc/version | awk -F' ' '{print $3}'`
 
 echo "kernel commit ID is $commit_id"
 
+lava_report "Prepare_cmd" "pass" ${commit_id} 
+
 lava_report "Prepare_test" "pass" ${commit_id} 
 
 # clean exit so lava-test can trust the results
